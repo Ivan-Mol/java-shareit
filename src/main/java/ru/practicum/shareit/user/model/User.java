b2c53704a49795310deefda369567cf5964ec763
Ivan-Mol <abyss.devotee@gmail.com>
2023-05-11 17:57:34 +0200

    ref

--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -21,6 +21,6 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
     private String email;
 }
