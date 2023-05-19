a73f88527da8527b34b2909ea43034ee2315ac68
Ivan-Mol <abyss.devotee@gmail.com>
2023-05-19 16:31:33 +0200

    chexkstyle fix2

--- src/test/java/ru/practicum/shareit/booking/service/BookingServiceIntegrationTest.java
+++ src/test/java/ru/practicum/shareit/booking/service/BookingServiceIntegrationTest.java
@@ -22,7 +22,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
-class BookingServiceImplTest {
+class BookingServiceIntegrationTest {
 
     @Autowired
     BookingService bookingService;
